1434ab6f48e4e3e03626d804d1a09d1763ec52aa
ethanhunt7089 <ethanhunt7089@gmail.com>
2025-07-09 01:23:09 +0700

    listen 0000

--- src/main.ts
+++ src/main.ts
@@ -85,7 +85,7 @@ async function bootstrap() {
 
   // Start Server
   const port = process.env.PORT || 3001;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
   console.log(`Swagger documentation is available at: ${await app.getUrl()}/api-docs`);
 }
